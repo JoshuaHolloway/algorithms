3257bda793417e384aaf033a76bfdf9bae1bbdd7
JoshuaHolloway <jhollow6@asu.edu>
2021-12-17 21:59:14 -0600

    unique elements string alg solution with a hash-map

--- 03/A/main.js
+++ 03/A/main.js
@@ -8,42 +8,50 @@
 
 // ==============================================
 
+// -O(n^2) time-complexity.
 function solution(str_1, str_2) {
-  const arr = [...str_1.split(''), ...str_2.split('')].sort();
+  const arr = [...str_1, ...str_2].sort();
 
-  console.log('arr: ', arr);
+  // console.log('arr: ', arr);
 
   let out = [];
   const N = arr.length;
+
+  out.push(arr[0]);
   for (let i = 0; i < N; ++i) {
-    let current = arr[i];
-
-    for (let j = i; j < N; ++j) {
-      console.log(
-        'i: ',
-        i,
-        '\tj:',
-        j,
-        '\tarr[i]: ',
-        arr[i],
-        '\tarr[j]: ',
-        arr[j]
-      );
-
-      if (current !== arr[j]) {
-        out.push(current);
-        console.log('i: ', i, '\tj:', j);
+    for (let j = i + 1; arr[i] !== arr[j] && j < N; ++j) {
+      // console.log('arr[i]: ', arr[i], '\tarr[j]: ', arr[j]);
+      out.push(arr[j]);
+      if (arr[i] !== arr[j]) {
         j = N;
       }
     }
   }
-  return out;
+  return out.join('');
+}
+
+// ==============================================
+
+// -O(n) time-complexity.
+// -This is the optimal solution.
+function solution2(str_1, str_2) {
+  const arr = [...str_1, ...str_2].sort();
+
+  let map = {};
+  for (let i = 0; i < arr.length; ++i) {
+    map[arr[i]] = arr[i];
+  }
+
+  let vals = Object.values(map);
+  return vals.join('');
 }
 
 // ==============================================
 
 // solution("aabbbcccdef", "xxyyzzz") -> "abcdefxyz"
 console.log(solution('aabbbcccdef', 'xxyyzzz')); // "abcdefxyz"
+console.log(solution2('aabbbcccdef', 'xxyyzzz')); // "abcdefxyz"
 
 // solution("abc", "abc") -> "abc"
-console.log('abc', 'abc'); // "abc"
+console.log(solution('abc', 'abc')); // "abc"
+console.log(solution2('abc', 'abc'));
