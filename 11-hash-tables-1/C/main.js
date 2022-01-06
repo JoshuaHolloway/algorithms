70197063b3fdfb02d28c5f55d981d022cfc882cf
JoshuaHolloway <jhollow6@asu.edu>
2022-01-06 16:01:25 -0600

    hash table 1

--- 11-hash-tables-1/C/main.js
+++ 11-hash-tables-1/C/main.js
@@ -28,6 +28,21 @@
 
 // Josh problem solving Step 1:   -Step through the provided example:
 //
+// Example 1:
+// Input: text = "mbxcdatlas"
+// lambda(xcts)
+// Output: 1
+//
+// Example 2:
+// Input: text = "lalaaxcmbdtsumbdav"
+// lambda(axcltsumbdav)
+// lambda(laaxcmbdtsuv)
+// Output: 2
+//
+// Example 3:
+// Input: text = "sctlamb"
+// Output: 0
+//
 // Josh problem solving step 2:   -formulate a solution. try to create a functional approach where each step is a function taking the output of the previous step as the input to the current step.
 // Step 1:  step_1_output = f1(input)
 // Step 2:  step_2_output = f2(step_1_output)
@@ -38,12 +53,60 @@
 
 // ==============================================
 
-const f1 = (input) => {};
+const f1 = (input) => {
+  // -Count the number of each character
+  // -Input: string
+  // -Output: Hash Table where keys are the letter and values are the number of times that character occured in the string.
+  let table = {};
+  for (let char of input) {
+    if (table[char]) {
+      ++table[char];
+    } else {
+      table[char] = 1;
+    }
+  }
+  return table;
+};
+
+// ==============================================
+
+const f2 = (table) => {
+  // -Count number of times ALL the letters {'l', 'a' (x2), 'm', 'b', 'd'} occur in the hash table
+
+  let count = 0;
+
+  while (true) {
+    if (
+      table['l'] &&
+      table['a'] &&
+      table['m'] &&
+      table['b'] &&
+      table['d'] &&
+      table['a']
+    ) {
+      for (let key in table) {
+        table[key]--;
+      }
+      ++count;
+    } else return count;
+  }
+};
 
 // ==============================================
 
 // Do tests:
 
-// Input:   ...
-// Output:  ...
-console.log();
+// Example 1:
+//  -Input:   text = "mbxcdatlas"
+//  -Output:  1
+console.log(f2(f1('mbxcdatlas')));
+
+// Example 2:
+//  -Input:   text = "lalaaxcmbdtsumbdav"
+//  -Output:  2
+console.log(f2(f1('lalaaxcmbdtsumbdav')));
+
+// Example 3:
+//  -Input: text = "sctlamb"
+//  -Output: 0
+console.log(f2(f1('sctlamb')));
