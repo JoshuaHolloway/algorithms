9b7e91c1e7e3d44ebafa042cdce53a92ce759364
JoshuaHolloway <jhollow6@asu.edu>
2022-01-20 02:47:01 -0600

    fixed .add() in binary search tree and added .find()

--- data-structures/2_trees/C-binary-search-tree/main.js
+++ data-structures/2_trees/C-binary-search-tree/main.js
@@ -3,11 +3,15 @@
 // ==============================================
 
 class Node {
+  // --------------------------------------------
+
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
+
+  // --------------------------------------------
 
   add(value) {
     if (this.value === null) {
@@ -15,35 +19,93 @@ class Node {
       return;
     }
 
-    const newNode = new Node(value);
     if (this.value < value) {
+      if (this.right) {
+        this.right.add(value);
+        return;
+      }
+
+      // -Only do this if we are at the leaf node
+      const newNode = new Node(value);
       this.right = newNode;
       return;
     }
 
     if (this.value > value) {
+      if (this.left) {
+        this.left.add(value);
+        return;
+      }
+
+      // -Only do this if we are at the leaf node
+      const newNode = new Node(value);
       this.left = newNode;
       return;
     }
   }
+
+  // --------------------------------------------
+
+  find(value) {
+    if (this.value === value) {
+      return this;
+    }
+
+    if (this.value < value && this.right) {
+      return this.right.find(value);
+    }
+
+    if (this.value > value && this.left) {
+      return this.left.find(value);
+    }
+  }
+
+  // --------------------------------------------
 }
 
 // ==============================================
 
 class Tree {
+  // --------------------------------------------
+
   constructor() {
     this.root = new Node(null);
   }
+
+  // --------------------------------------------
 
   add(value) {
     this.root.add(value);
   }
 
+  // --------------------------------------------
+
   remove(value) {
     this.root.remove(value);
   }
 
+  // --------------------------------------------
+
   find(value) {
     return this.root.find(value);
   }
+
+  // --------------------------------------------
 }
+
+// ==============================================
+
+// -This implemenation doesn't work.
+
+const tree = new Tree();
+tree.add(10);
+tree.add(5);
+tree.add(2); // -Overwrites 6 on right!!!
+tree.add(6);
+tree.add(20);
+tree.add(25);
+tree.add(39);
+console.log(tree);
+console.log('6: ', tree.find(6));
+console.log('7: ', tree.find(7));
+console.log('39: ', tree.find(39));
