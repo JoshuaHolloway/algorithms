a5b37cd9790f794ffd3709f81f9af4cacfdca33d
JoshuaHolloway <jhollow6@asu.edu>
2022-01-21 14:19:05 -0600

    updated directory structure

---
+++ sprint-challenges/3/A/main.js
@@ -0,0 +1,27 @@
+// -Optimality requirements:
+//  --O(l.length) time complexity
+//  --O(1) space complexity
+
+// Given a singly linked list, reverse and return it.
+
+// Example
+// For l = [1, 2, 3, 4, 5], the output should be
+// solution(l) = [5, 4, 3, 2, 1].
+
+// Input/Output
+// [execution time limit] 4 seconds (js)
+
+// [input] linkedlist.integer l
+// A singly linked list of integers.
+
+// Guaranteed constraints:
+// 0 ≤ l.length ≤ 105,
+// -109 ≤ l.value ≤ 109.
+
+// [output] linkedlist.integer
+// Reversed l.
+
+// -Follow up questions:
+//  --Explain the time and space complexity of your solution.
+//  --Is your solution optimal?
+//  --If not, how could you imporove the time/space-complexity of your solution?
---
+++ sprint-challenges/3/C/main.js
@@ -0,0 +1,27 @@
+// Given a binary tree, write a function that inverts the tree.
+
+//Example:
+// Input:
+//      6
+//    /   \
+//   4     8
+//  / \   / \
+// 2   5 7   9
+
+// Output:
+//      6
+//    /   \
+//   8     4
+//  / \   / \
+// 9   7 5   2
+
+// [execution time limit] 4 seconds (js)
+
+// [input] tree.integer root
+
+// [output] tree.integer
+
+// -Follow up questions:
+//  --Explain the time and space complexity of your solution.
+//  --Is your solution optimal?
+//  --If not, how could you imporove the time/space-complexity of your solution?
